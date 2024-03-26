bcecb4d07d7eb93c82fe08ea65cba2b2c4dc6402
konrad-bak <bak.konrad@gmail.com>
2024-03-26 21:51:59 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const { readFile } = require('fs').promises;
+
+const app = express();
+
+app.get('/', async (request, response) => {
+  response.send(await readFile('./home.html', 'utf8'));
+});
